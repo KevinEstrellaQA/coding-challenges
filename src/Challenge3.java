c3367424dfde24db20bd6e5631333b8db0308170
kevin <kevin.e.estrella@gmail.com>
2025-08-03 20:07:40 -0600

    Challenge 3 - Sucesión de Fibonacci

---
+++ src/Challenge3.java
@@ -0,0 +1,31 @@
+/*
+ * Escribe un programa que imprima los 50 primeros números de la sucesión
+ * de Fibonacci empezando en 0.
+ * - La serie Fibonacci se compone por una sucesión de números en
+ *   la que el siguiente siempre es la suma de los dos anteriores.
+ *   0, 1, 1, 2, 3, 5, 8, 13...
+ */
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Challenge3 {
+    public static void main(String[] args) {
+        List<Long> fibonacci = new ArrayList<>();
+        int count = 0;
+        while (fibonacci.size() < 50){
+            if(!fibonacci.isEmpty() && fibonacci.size() >= 2){
+                fibonacci.add(fibonacci.get(count-2) + fibonacci.get(count-1));
+            }else{
+                fibonacci.add((long) count);
+            }
+            count++;
+        }
+        System.out.println("\nPrimeros 50 números de la sucesión de Fibonacci:\n");
+        System.out.println(fibonacci);
+        System.out.println("\nLista de elementos:\n");
+        for(long number : fibonacci){
+            System.out.println(number);
+        }
+    }
+}
