adc09539731a389c98446d66cd923120d2800b77
kevin <kevin.e.estrella@gmail.com>
2025-08-14 17:33:39 -0600

    Challenge 9 - Decimal a Binario

--- src/Challenge9.java
+++ src/Challenge9.java
@@ -3,9 +3,7 @@
  * decimal a binario sin utilizar funciones propias del lenguaje que lo hagan directamente.
  */
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+
 
 public class Challenge9 {
 
@@ -14,18 +12,13 @@ public class Challenge9 {
     }
 
     public static String binary(int num){
-        List<String> binary = new ArrayList<>(Arrays.asList("0", "0", "0", "0", "0", "0", "0", "0"));
-        int count = 7;
-        while(!(num / 2 == 0)){
-            if(num % 2 == 1){
-                binary.add(count, "1");
-            }else{
-                binary.add(count, "0");
-            }
-            count--;
-            num = num / 2;
+        if(num == 0) return "0";
+        String binary = "";
+        while(num > 0){
+            int res = num % 2;
+            binary = res + binary;
+            num /= 2;
         }
-        System.out.println(binary);
-        return binary.toString();
+        return binary;
     }
 }
